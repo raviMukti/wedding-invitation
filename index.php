a791998dc3be41a157affb6f48a32f26e8120c65
raviMukti <shinjiravi@gmail.com>
2022-05-21 20:45:41 +0700

    Finishing Project

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+ini_set('max_execution_time', 4000);
+ini_set('display_errors',1);
+ini_set('mysql.connect_timeout', 4000);
+ini_set('default_socket_timeout', 4000);
+
+include_once "index.html";
